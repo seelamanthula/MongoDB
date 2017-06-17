25e31c35cf1177b3b75efb9a4f89b7aeb0ff29dc
seelamanthula <hseelamanthula@albany.edu>
2017-06-17 02:01:11 -0400

    Material

---
+++ .mongorc.js
@@ -0,0 +1,4 @@
+var compliment = ["attractive", "intelligent", "like Batman"];
+var index = Math.floor(Math.random()*3);
+print("Hello..! You are looking particularly "+compliment[index]+" today!");
+
